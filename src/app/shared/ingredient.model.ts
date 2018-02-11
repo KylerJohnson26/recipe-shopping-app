c5792d3a0a44f447f5c6d7950333ca8eb44581a4
kljohnson2 <kylerljohnson@icloud.com>
2018-02-11 15:43:55 -0500

    adding ingredient model and filling out shopping list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // this one-liner accomplishes the same thing as the commented code below
+    constructor(public name: string, public amount: number){}
+
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this. amount = amount;
+    // }
+}
